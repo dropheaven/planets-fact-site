5d187f107719bac20379cc72ed02c55a8b372f40
Saad <dropheaven@live.com>
2021-08-10 14:10:29 -0400

    add design colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,20 @@ module.exports = {
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    colors: {
+        white: '#FFFFFF',
+        darkblue: '#070724',
+        grayishblue: '#38384F',
+        gray: '#838391',
+        skyblue: '#419EBB',
+        orange: '#EDA249',
+        purple: '#6F2ED6',
+        lightred: '#D14C32',
+        red: '#D83A34',
+        orange: '#CD5120',
+        aqua: '#1EC2A4',
+        blue: '#2D68F0'
+    }
   },
   variants: {
     extend: {},
