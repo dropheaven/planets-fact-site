1fa6161a80a008b7b6f3e91016e0ac17251c679c
Saad <dropheaven@live.com>
2021-08-12 20:29:04 -0400

    move colors and fontsizes inside of extend and add group hover for translate property

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,28 +1,35 @@
 module.exports = {
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
-  darkMode: false, // or 'media' or 'class'
-  theme: {
-    colors: {
-        white: '#FFFFFF',
-        darkblue: '#070724',
-        grayishblue: '#38384F',
-        gray: '#838391',
-        skyblue: '#419EBB',
-        orange: '#EDA249',
-        purple: '#6F2ED6',
-        lightred: '#D14C32',
-        red: '#D83A34',
-        orange: '#CD5120',
-        aqua: '#1EC2A4',
-        blue: '#2D68F0'
-    },
-    fontFamily: {
-        antonio: 'Antonio, sans-serif',
-        spartan: 'Spartan, sans-serif'
-    }
-  },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
-}
+	purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+	darkMode: false, // or 'media' or 'class'
+	theme: {
+		fontFamily: {
+			antonio: "Antonio, sans-serif",
+			spartan: "Spartan, sans-serif",
+		},
+		extend: {
+			fontSize: {
+				"2xs": ".7rem",
+			},
+			colors: {
+				white: "#FFFFFF",
+				darkblue: "#070724",
+				grayishblue: "#38384F",
+				// gray: "#838391",
+				skyblue: "#419EBB",
+				lightorange: "#EDA249",
+				purple: "#6F2ED6",
+				lightred: "#D14C32",
+				red: "#D83A34",
+				orange: "#CD5120",
+				aqua: "#1EC2A4",
+				blue: "#2D68F0",
+			},
+		},
+	},
+	variants: {
+		extend: {
+			translate: ["group-hover"],
+		},
+	},
+	plugins: [],
+};
